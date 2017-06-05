8def13b7ebdea37542e46f23154c2faf0b385e43
JukioKe <jukka.satukangas@gmail.com>
2017-06-05 23:10:02 +0300

    Changes: - Started on working the Numbers Activity

--- app/src/main/java/com/example/android/learnfinnish/NumbersActivity.java
+++ app/src/main/java/com/example/android/learnfinnish/NumbersActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.learnfinnish;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> numberWords = new ArrayList<>();
+        String[] words = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+        numberWords.addAll(Arrays.asList(words));
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootview);
+
+
+        for (int index = 0; index < numberWords.size(); index++) {
+            TextView wordView = new TextView(this);
+            wordView.setText(numberWords.get(index));
+            rootView.addView(wordView);
+
+        }
+
     }
 }
