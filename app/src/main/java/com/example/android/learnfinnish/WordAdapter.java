9e5374c1316adae94c0c47363c1a8c24c0462089
JukioKe <jukka.satukangas@gmail.com>
2017-06-16 00:03:04 +0300

    Changes: - Modified list_item XML-file so it can show the image as well - Corrected some wrong lines in PhrasesActivity - Added new line in FamilyMembersActivity

--- app/src/main/java/com/example/android/learnfinnish/WordAdapter.java
+++ app/src/main/java/com/example/android/learnfinnish/WordAdapter.java
@@ -19,7 +19,7 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-// Check if the existing view is being reused, otherwise inflate the view
+        // Check if the existing view is being reused, otherwise inflate the view
         View listItemView = convertView;
         if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
