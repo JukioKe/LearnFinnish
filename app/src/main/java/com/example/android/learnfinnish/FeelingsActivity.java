a2adc9ed3a160ee457a0fd7342208f502e07ad6a
JukioKe <jukka.satukangas@gmail.com>
2017-06-18 14:58:47 +0300

    Changes: - Added images in NumbersActivity - Added some of the images of the FeelingsActivity

--- app/src/main/java/com/example/android/learnfinnish/FeelingsActivity.java
+++ app/src/main/java/com/example/android/learnfinnish/FeelingsActivity.java
@@ -16,27 +16,28 @@ public class FeelingsActivity extends AppCompatActivity {
 
         ArrayList<Word> feelingsWords = new ArrayList<Word>();
 
-        feelingsWords.add(new Word("iloinen", "happy"));
-        feelingsWords.add(new Word("surullinen", "sad"));
-        feelingsWords.add(new Word("hyvä olo", "good feel"));
+        feelingsWords.add(new Word("iloinen", "happy", R.drawable.feeling_happy));
+        feelingsWords.add(new Word("surullinen", "sad", R.drawable.feeling_sad));
+        feelingsWords.add(new Word("hyvä olo", "good feel", R.drawable.feeling_good_feel));
         feelingsWords.add(new Word("epätoivo", "despair"));
-        feelingsWords.add(new Word("hämmennys", "bewilderment"));
+        feelingsWords.add(new Word("hämmennys", "bewilderment", R.drawable.feeling_bewilderment));
         feelingsWords.add(new Word("häpeä", "shame"));
         feelingsWords.add(new Word("ikävä", "tedious"));
         feelingsWords.add(new Word("intohimo", "passion"));
         feelingsWords.add(new Word("jännitys", "excitement"));
-        feelingsWords.add(new Word("kaipaus", "yearning"));
-        feelingsWords.add(new Word("kauhu", "fright"));
+        feelingsWords.add(new Word("kaipaus", "yearning", R.drawable.feeling_yearning));
+        feelingsWords.add(new Word("kauhu", "fright", R.drawable.feeling_fright));
         feelingsWords.add(new Word("kiinnostus", "interest"));
-        feelingsWords.add(new Word("kunnioitus", "respect"));
+        feelingsWords.add(new Word("kunnioitus", "respect", R.drawable.feeling_respect));
         feelingsWords.add(new Word("levottomuus", "anxiety"));
         feelingsWords.add(new Word("luottamus", "confidence"));
-        feelingsWords.add(new Word("onni", "luck"));
-        feelingsWords.add(new Word("rakkaus", "love"));
+        feelingsWords.add(new Word("onni", "luck", R.drawable.feeling_luck));
+        feelingsWords.add(new Word("rakkaus", "love", R.drawable.feeling_love));
         feelingsWords.add(new Word("syyllisyys", "guilt"));
         feelingsWords.add(new Word("ujo", "shy"));
         feelingsWords.add(new Word("viha", "anger"));
         feelingsWords.add(new Word("vapaa", "free"));
+        feelingsWords.add(new Word("kipeä", "sick", R.drawable.feeling_sick));
         feelingsWords.add(new Word("väsymys", "tiredness"));
         feelingsWords.add(new Word("yksinäisyys", "loneliness"));
         feelingsWords.add(new Word("ylpeys", "pride"));
