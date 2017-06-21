d68d38fc8c6be6349e8e84417e581d1f95b965ba
JukioKe <jukka.satukangas@gmail.com>
2017-06-21 23:09:52 +0300

    Changes: - Finished adding SportsActivity images

--- app/src/main/java/com/example/android/learnfinnish/SportsActivity.java
+++ app/src/main/java/com/example/android/learnfinnish/SportsActivity.java
@@ -19,26 +19,27 @@ public class SportsActivity extends AppCompatActivity {
         sportWords.add(new Word("syötä", "pass", R.drawable.sports_pass));
         sportWords.add(new Word("laukaise, vedä", "shoot", R.drawable.sports_shoot));
         sportWords.add(new Word("joukkue", "team", R.drawable.sports_team));
-        sportWords.add(new Word("vierasjoukkue, vastustaja", "away team"));
-        sportWords.add(new Word("kotijoukkue", "home team"));
         sportWords.add(new Word("pelata", "play", R.drawable.sports_play));
         sportWords.add(new Word("voitto", "victory", R.drawable.sports_trophy));
         sportWords.add(new Word("tappio", "defeat", R.drawable.sports_defeat));
         sportWords.add(new Word("urheilija", "athlete", R.drawable.sports_athlete));
-        sportWords.add(new Word("pallo", "ball"));
+        sportWords.add(new Word("pallo", "ball", R.drawable.sports_ball));
         sportWords.add(new Word("maila", "racquet", R.drawable.sports_raquet));
-        sportWords.add(new Word("pelaaja", "player"));
-        sportWords.add(new Word("maalivahti", "goalkeeper"));
-        sportWords.add(new Word("kenttä", "field"));
+        sportWords.add(new Word("pelaaja", "player", R.drawable.sports_play));
+        sportWords.add(new Word("maalivahti", "goalkeeper", R.drawable.sports_goalkeeper));
+        sportWords.add(new Word("kenttä", "field", R.drawable.sports_field));
         sportWords.add(new Word("maali", "goal", R.drawable.sports_goal));
         sportWords.add(new Word("verkko", "net", R.drawable.sports_net));
-        sportWords.add(new Word("valmentaja", "coach"));
-        sportWords.add(new Word("piste", "point"));
+        sportWords.add(new Word("valmentaja", "coach", R.drawable.sports_coach));
+        sportWords.add(new Word("piste", "point", R.drawable.sports_point));
         sportWords.add(new Word("mitali", "medal", R.drawable.sports_medal));
         sportWords.add(new Word("palkinto", "trophy", R.drawable.sports_trophy));
-        sportWords.add(new Word("paitsio", "offside"));
-        sportWords.add(new Word("tuomari", "referee"));
-        sportWords.add(new Word("kunto", "fitness"));
+        sportWords.add(new Word("paitsio", "offside", R.drawable.sports_offside));
+        sportWords.add(new Word("tuomari", "referee", R.drawable.sports_referee));
+        sportWords.add(new Word("vierasjoukkue, vastustaja", "away team", R.drawable.sports_away_team));
+        sportWords.add(new Word("kotijoukkue", "home team", R.drawable.sports_team));
+        sportWords.add(new Word("pelata", "play", R.drawable.sports_play));
+        sportWords.add(new Word("kunto", "fitness", R.drawable.sports_fitness));
 
 
         WordAdapter adapter = new WordAdapter(this, sportWords);
